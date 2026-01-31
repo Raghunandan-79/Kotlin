beb43cceff869137abed7485324d9aef34562751
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-31 16:44:30 +0530

    Generating random numbers

---
+++ src/main/kotlin/section3UserInput/GenratingRandomNumber.kt
@@ -0,0 +1,11 @@
+package org.example.section3UserInput
+
+import kotlin.random.Random
+
+fun main() {
+    println(Random.nextInt())
+    println(Random.nextInt(6))
+    println(Random.nextInt(1, 10))
+
+    println(Random.nextDouble(300.0))
+}
