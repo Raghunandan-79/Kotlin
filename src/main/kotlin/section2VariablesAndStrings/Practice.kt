f0f00a41ef54833a48daf28268900e0e47ffe9a5
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-31 15:23:19 +0530

    practice strings and variables

---
+++ src/main/kotlin/section2VariablesAndStrings/Practice.kt
@@ -0,0 +1,17 @@
+package org.example.section2VariablesAndStrings
+
+fun main(args: Array<String>) {
+    val catName = "My cat's name is \"Fluffy\""
+    println(catName)
+
+    val slashes = "Two types of slashes: \\ and /"
+    println(slashes)
+
+    val url = "http://www.google.com"
+    println(url)
+
+    // The below code will print a bunny
+    println("(\\(\\\n" +
+            "(-.-)\n" +
+            "O_(\")(\")")
+}
