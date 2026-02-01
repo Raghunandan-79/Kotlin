68c4721d79ff2f24ffcc1b141841eff0d488ad80
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-02-01 20:38:06 +0530

    Augmented Assignment Operators

---
+++ src/main/kotlin/section5OperatorsAndBooleans/AugmentedAssignment.kt
@@ -0,0 +1,20 @@
+package org.example.section5OperatorsAndBooleans
+
+fun main() {
+    var bikes = 5
+
+    bikes += 1
+    println(bikes)
+
+    bikes -= 1
+    println(bikes)
+
+    bikes *= 2
+    println(bikes)
+
+    bikes /= 2
+    println(bikes)
+
+    bikes %= 2
+    println(bikes)
+}
