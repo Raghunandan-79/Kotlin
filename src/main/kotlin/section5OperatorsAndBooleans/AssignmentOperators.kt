120c5cebcb2497ac873415042d0d457728811e46
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-02-01 19:31:22 +0530

    Assignment Operators

---
+++ src/main/kotlin/section5OperatorsAndBooleans/AssignmentOperators.kt
@@ -0,0 +1,16 @@
+package org.example.section5OperatorsAndBooleans
+
+fun main() {
+    val dozen = 12
+    val months = dozen
+    println(months)
+
+    val userName = "Lucy"
+    val greeting = "Hello $userName"
+    println(greeting)
+
+    val products = 3
+    val price = 9.99
+    val total = "Your total is ${products * price}"
+    println(total)
+}
