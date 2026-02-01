af070e688191662714dbc65f70d0cb15cb2eae82
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-02-02 05:14:06 +0530

    Comparison Operators

---
+++ src/main/kotlin/section5OperatorsAndBooleans/ComparisonOperators.kt
@@ -0,0 +1,13 @@
+package org.example.section5OperatorsAndBooleans
+
+fun main() {
+    val a = 5
+    val b = 2
+
+    println("$a > $b = ${a > b}")
+    println("$a < $b = ${a < b}")
+    println("$a >= $b = ${a >= b}")
+    println("$a <= $b = ${a <= b}")
+    println("$a == $b = ${a == b}")
+    println("$a != $b = ${a != b}")
+}
