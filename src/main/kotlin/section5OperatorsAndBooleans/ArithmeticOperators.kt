57828240398cde2fa4ce9996d9da2003bf69026e
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-02-01 18:37:41 +0530

    Arithmetic Operators

---
+++ src/main/kotlin/section5OperatorsAndBooleans/ArithmeticOperators.kt
@@ -0,0 +1,32 @@
+package org.example.section5OperatorsAndBooleans
+
+fun main() {
+    // Addition
+    println(5 + 2)
+    println("Hi" + " " + "John")
+
+    // Subtraction
+    println(5 - 2)
+
+    // Multiplication
+    println(5 * 2)
+
+    // Division
+    println(5 / 2)
+    println(5f / 2f)
+
+    // Modulus (Remainder)
+    println(5 % 2)
+
+    var cats = 5
+    println(cats++) // 5
+    println(cats) // 6
+    println(++cats) // 7
+
+    println(cats) // 7
+
+    println(cats--) // 7
+    println(cats) // 6
+    println(--cats) // 5
+    println(cats) // 5
+}
